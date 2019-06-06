ea49fed6a12f062511cae721260dc029ac60033d
Yacine Ferhat <Yacine.frhat@gmail.com>
2019-06-06 23:12:51 +0200

    première sauvegarde

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <app-new-quotes @quoteAdded="newQuote"></app-new-quotes>
+    <app-Quote-Grid :quotes="quotes"></app-Quote-Grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a quote to delete it!
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid';
+  import NewQuotes from './components/NewQuotes';
+
+  export default {
+    data : function(){
+      return{
+        quotes : [
+          'Just a quote'
+        ],
+        maxQuotes : 10
+      }
+    },
+    components:{
+      appQuoteGrid : QuoteGrid,
+      appNewQuotes : NewQuotes
+    },
+    methods : {
+      newQuote(quote){
+        this.quotes.push(quote);
+      }
+    }
+  }
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-sn-6 col-md-4 col-lg-3">
+        <div class="pannel pannel-default">
+            <div class="pannel-body quote">
+                <slot>
+                    
+                </slot>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+
+</script>
+
+<style>
+    .pannel-body{
+        font-family: 'Indie Flower', cursive;
+        font-size: 24px;
+        color : #6e6e6e;   
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover{
+        background-color: #ffe2e2;
+    }
+</style>
